0b745acc3cc6690d1b58de4b2b3e81dd4d614834
Karol Banat <karol.banat@gmail.com>
2023-08-01 07:41:08 +0200

    Base Header Component

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 <header>
-	<a href="#"><img src="assets/images/logo.svg" alt="Home page" /></a>
+	<a href="/"><img src="assets/images/logo.svg" alt="Home page" /></a>
 </header>
 
 <style></style>
