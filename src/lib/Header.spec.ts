078d46fc684f77f84eed47570c33d4614aad1817
Karol Banat <karol.banat@gmail.com>
2023-08-01 07:40:05 +0200

    Header tests

---
+++ src/lib/Header.spec.ts
@@ -0,0 +1,17 @@
+import { describe, test, expect } from 'vitest';
+import { render } from '@testing-library/svelte';
+import Header__SvelteComponent_ from './Header.svelte';
+
+describe('Header Component', () => {
+	test('Should render logo', () => {
+		const header = render(Header__SvelteComponent_);
+		const logo = header.getByRole('img');
+		expect(logo).toBeTruthy();
+	});
+
+	test('Should render logo with alt', () => {
+		const header = render(Header__SvelteComponent_);
+		const logo = header.getAllByAltText('Home page');
+		expect(logo).toBeTruthy();
+	});
+});
