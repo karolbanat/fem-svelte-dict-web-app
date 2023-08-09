6a2874fb5cbf7f26de5aca87f9ca8c414a40e374
Karol Banat <karol.banat@gmail.com>
2023-08-09 19:14:16 +0200

    Added Wrapper component

---
+++ src/lib/Wrapper.svelte
@@ -0,0 +1,14 @@
+<script lang="ts"></script>
+
+<div class="wrapper">
+	<slot />
+</div>
+
+<style>
+	.wrapper {
+		--max-width: 46rem;
+		--padding-inline: clamp(var(--spacer-rem-600), 5.5vw, var(--spacer-rem-900));
+		margin-inline: auto;
+		width: min(var(--max-width), 100% - 2 * var(--padding-inline));
+	}
+</style>
