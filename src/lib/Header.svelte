ee3e09888c30b077583e8f9b77ff02bd64b9b898
Karol Banat <karol.banat@gmail.com>
2023-08-09 19:16:59 +0200

    Added Wrapper around Header and added padding

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,7 +1,27 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Preferences from './Preferences.svelte';
+	import Wrapper from './Wrapper.svelte';
+</script>
 
-<header>
-	<a href="/"><img src="assets/images/logo.svg" alt="Home page" /></a>
-</header>
+<Wrapper>
+	<header>
+		<a href="/"><img src="assets/images/logo.svg" alt="Home page" /></a>
+		<Preferences />
+	</header>
+</Wrapper>
 
-<style></style>
+<style>
+	header {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+
+		padding-block-start: var(--spacer-rem-600);
+	}
+
+	@media only screen and (min-width: 48em) {
+		header {
+			padding-block-start: 3.625rem;
+		}
+	}
+</style>
