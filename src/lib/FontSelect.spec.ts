9e5a121851184f9ad8730ba6809de10a2c5a2a2a
Karol Banat <karol.banat@gmail.com>
2023-08-15 13:32:26 +0200

    Add font change to tests

--- src/lib/FontSelect.spec.ts
+++ src/lib/FontSelect.spec.ts
@@ -15,4 +15,20 @@ describe('Font Select', () => {
 		expect(screen.getByRole('list').childNodes.length).toBe(3);
 		expect(screen.getAllByRole('radio').length).toBe(3);
 	});
+
+	test('Should change font when radio button selected', async () => {
+		render(FontSelect__SvelteComponent_);
+		const button: HTMLButtonElement = document.querySelector('.current-font');
+		await fireEvent.click(button);
+
+		const monoOption = screen.getByLabelText('Mono');
+		await fireEvent.click(monoOption);
+		expect(button.textContent).toEqual('Current font: Mono');
+		const serifOption = screen.getByLabelText('Serif');
+		await fireEvent.click(serifOption);
+		expect(button.textContent).toEqual('Current font: Serif');
+		const sansOption = screen.getByLabelText('Sans Serif');
+		await fireEvent.click(sansOption);
+		expect(button.textContent).toEqual('Current font: Sans Serif');
+	});
 });
