d3d4327d92608fcc3777b0fdac7b7d6423bac8b9
Karol Banat <karol.banat@gmail.com>
2023-09-25 12:14:22 +0200

    Font change

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 
 <svelte:head>
 	<link rel="stylesheet" href={`./css/themes/${$preferences.theme}-theme.css`} />
+	<link rel="stylesheet" href={`./css/fonts/${$preferences.font}-font.css`} />
 </svelte:head>
 
 <Header />
